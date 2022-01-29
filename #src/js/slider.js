71ee71ef3ae7784990673827589bc44265623a9a
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2022-01-29 15:03:24 +0300

    скрипты окна  просмотра изображения

--- #src/js/slider.js
+++ #src/js/slider.js
@@ -21,3 +21,49 @@ carousel.querySelector('.slide--next').onclick = function () {
     position = Math.max(position, -width * (listElems.length - count));
     list.style.marginLeft = position + 'px';
 };
+
+
+
+// const popupSliderOpen = document.querySelector('.picture-popup');
+
+const popupSlider = document.querySelector('.picture-popup');
+const popupSliderClose = document.querySelector('.picture-popup__button-close');
+const popupSliderOpen = document.querySelectorAll('.gallery__image');
+const overlay = document.querySelector('.overlay');
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const showPopupSlide = () => {
+
+    overlay.classList.add('overlay--active');
+    popupSlider.classList.add('picture-popup--active');
+
+    const onKeydown = (evt) => {
+        if (isEscapeKey(evt)) {
+            overlay.classList.remove('overlay--active');
+            popupSlider.classList.remove('picture-popup--active');
+        }
+    };
+
+    overlay.addEventListener('click', (event) => {
+        if (event.target.className != 'popup-addnote') {
+            overlay.classList.remove('overlay--active');
+            popupSlider.classList.remove('picture-popup--active');
+            document.removeEventListener('keydown', onKeydown);
+        };
+
+    }, { once: true });
+
+    document.addEventListener('keydown', onKeydown, { once: true });
+
+    popupSliderClose.addEventListener('click', () => {
+        overlay.classList.remove('overlay--active');
+        popupSlider.classList.remove('picture-popup--active');
+        document.removeEventListener('keydown', onKeydown);
+    }, { once: true });
+};
+
+popupSliderOpen.forEach(item => {
+    item.addEventListener('click', event => {
+        showPopupSlide();
+    })
+})
